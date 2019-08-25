02dfd4be9e6e6b8208585a4a73fa7b285b53dc44
Kevin Lluch Martinez <lluchmk@gmail.com>
2019-08-25 18:23:00 +0200

    Update ability creation to include the userId.

--- Abilities/Abilities.Application/Abilities/Commands/CreateAbility/CreateAbilityCommandHandler.cs
+++ Abilities/Abilities.Application/Abilities/Commands/CreateAbility/CreateAbilityCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Abilities.Application.Abilities.Enums;
 using Abilities.Application.Interfaces.Repositories;
+using Abilities.Application.Interfaces.Services;
 using Abilities.Domain.Entities;
 using AutoMapper;
 using MediatR;
@@ -9,37 +10,41 @@ using System.Threading.Tasks;
 
 namespace Abilities.Application.Abilities.Commands.CreateAbility
 {
-    class CreateAbilityCommandHandler : IRequestHandler<CreateAbilityCommand, int>
+    public class CreateAbilityCommandHandler : IRequestHandler<CreateAbilityCommand, int>
     {
         private readonly IAbilitiesRepository _repository;
         private readonly IMapper _mapper;
+        private readonly IUsersService _usersService;
 
-        public CreateAbilityCommandHandler(IAbilitiesRepository repository, IMapper mapper)
+        public CreateAbilityCommandHandler(IAbilitiesRepository repository, IMapper mapper, IUsersService usersService)
         {
             _repository = repository;
             _mapper = mapper;
+            _usersService = usersService;
         }
 
         public async Task<int> Handle(CreateAbilityCommand command, CancellationToken cancellationToken)
         {
-            int createdId;
+            BaseAbility ability;
             switch (command.Type)
             {
                 case AbilityType.Ability:
-                    var ability = _mapper.Map<Ability>(command);
-                    createdId = await _repository.Create(ability, cancellationToken);
+                    ability = _mapper.Map<Ability>(command);
                     break;
                 case AbilityType.MysticalPower:
-                    var mysticalPower = _mapper.Map<MysticalPower>(command);
-                    createdId = await _repository.Create(mysticalPower, cancellationToken);
+                    ability = _mapper.Map<MysticalPower>(command);
                     break;
                 case AbilityType.Ritual:
-                    var ritual = _mapper.Map<Ritual>(command);
-                    createdId = await _repository.Create(ritual, cancellationToken);
+                    ability = _mapper.Map<Ritual>(command);
                     break;
                 default:
                     throw new NotImplementedException("No handler for unrecognized ability type"); // TODO: Better exception
             }
+
+            var userId = _usersService.GetUserId();
+            ability.UserId = userId;
+
+            var createdId = await _repository.Create(ability, cancellationToken);
             return createdId;
         }
     }
